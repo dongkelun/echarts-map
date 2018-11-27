4301bc0edf270898cad17777206aa4e8bb29b901
dongkelun <dongkelun@xlpayment.com>
2018-11-27 11:43:50 +0800

    first commit

---
+++ js/rem.js
@@ -0,0 +1,13 @@
+//监控页面分辨率变化，修改对应的字体大小
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 18 * (clientWidth / 1920) + 'px'
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window); 
